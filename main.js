5bb244e38b3ddf279ed62082e14f81f0164c6d63
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-03-05 17:34:45 +0200

    added validation lib

---
+++ main.js
@@ -0,0 +1,93 @@
+
+function validateField(inputField, OKStatus, NOKStatus, validationFunc){
+        const isValid = validationFunc(inputField.value);
+        changeStatusOneField(OKStatus, NOKStatus, isValid);
+    }
+
+function changeStatusOneField (OK, NOK,  condition){
+    OK.style.display = condition ? 'flex': 'none';
+    NOK.style.display = condition ? 'none': 'flex';
+}
+
+
+const notEmptyField = document.getElementById("empty-field-input");
+const notEmptyFieldStatus = {
+    OK: document.getElementById("empty-input-ok"),
+    NOK:  document.getElementById("empty-input-not-ok")
+};
+notEmptyField.addEventListener(
+    "keyup", 
+        validateField(
+            notEmptyField, 
+            notEmptyFieldStatus.OK, 
+            notEmptyFieldStatus.NOK, 
+            notEmptyValidation
+            ));
+            
+const passwordField = document.getElementById("password-input");
+const passStatus = {
+    OK: document.getElementById("pass-ok"),
+    NOK: document.getElementById("pass-not-ok"),
+};
+function validatePassword (){
+    validateField(passwordField, passStatus.OK, passStatus.NOK, isComplexPassword);
+}
+
+
+const phoneNumberField = document.getElementById("phone-number-input");
+const phoneNumberStatus = {
+    OK: document.getElementById("phone-number-ok"),
+    NOK: document.getElementById("phone-number-not-ok"),
+};
+function validatePhoneNumber (){
+    validateField(phoneNumberField, phoneNumberStatus.OK, phoneNumberStatus.NOK, isValidPhoneNumber);
+}
+
+
+const emailField = document.getElementById("email-input");
+const emailStatus = {
+    OK: document.getElementById("email-ok"),
+    NOK: document.getElementById("email-not-ok"),
+};
+function validateEmail(){
+    validateField(emailField, emailStatus.OK, emailStatus.NOK, isValidEmail);
+}
+
+
+const creditCardField = document.getElementById("credit-card-input");
+const creditCardStatus = {
+    OK: document.getElementById("credit-card-ok"),
+    NOK: document.getElementById("credit-card-not-ok"),
+};
+function validateCreditCard(){
+    validateField(creditCardField, creditCardStatus.OK, creditCardStatus.NOK, isValidCreditCard);
+}
+
+
+const inRangeStatus = {
+    OK: document.getElementById("in-range-ok"),
+    NOK: document.getElementById("in-range-not-ok"),
+};
+let valuesOfRange = [];
+
+function updateArray(array, idx, value){
+    array[idx]=+value;
+}
+
+function changeStatusRange (array, statusObject) {
+    statusObject.OK.style.display = isInRange(...array) ? 'flex': 'none';
+    statusObject.NOK.style.display = isInRange(...array) ? 'none': 'flex';
+}
+
+function validateInRange(){
+    rangeInputs = document.querySelectorAll('input[type="number"]');
+    rangeInputs.forEach(
+        function(input, idx) {
+            input.addEventListener(
+                "keyup", 
+                function () {
+                    updateArray(valuesOfRange, idx, input.value);
+                    changeStatusRange(valuesOfRange, inRangeStatus);
+                });
+    });
+}
---
+++ validationLib.js
@@ -0,0 +1,56 @@
+function notEmptyValidation(value) {
+    if (value == null || value.trim() === '') {
+      return false;
+    }
+    if (value.trim() === 'false') {
+      return false;
+    }
+    return true;
+  }
+
+function isValidEmail (email) {
+    let emailRegex = /^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+/** 
+    * Function validates password input
+    * Password should contain at least one uppercase letter
+    * Password should contain at least one lowercase letter
+    * Password should contain at least one digit
+    * Password should contain at least one special character (^~@$!%*#?&)
+    * Password should contain at least 8 characters
+    * @param {string} password - user's input password
+    * @returns {boolean} true/false if password valid or not
+*/ 
+function isComplexPassword (password) {
+    let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[/^~@$!%*#?&])[A-Za-z\d~^@$!%*#?&]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function isValidPhoneNumber (phoneNumber) {
+    let phoneNumberRegex = /^\d{10}$/;
+    return phoneNumberRegex.test(phoneNumber);
+}
+
+function isValidCreditCard (creditCardNumber) {
+    const creditCardsRegexExpressions = {
+        mastercard: /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/,
+        americanExpress: /^3[47][0-9]{13}$/,
+        visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+        discover: /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/,
+        maestro: /^(5018|5081|5044|5020|5038|603845|6304|6759|676[1-3]|6799|6220|504834|504817|504645)[0-9]{8,15}$/,
+        jcb: /^(?:2131|1800|35[0-9]{3})[0-9]{11}$/,
+        diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
+    };
+    for (let creditcard in creditCardsRegexExpressions) {
+        console.log(creditCardsRegexExpressions[creditcard]);
+        if (creditCardsRegexExpressions[creditcard].test(creditCardNumber)){
+            return true;
+        }
+    }
+}
+
+function isInRange(min, max, number) {
+    return number >= min && number <= max;
+  }
