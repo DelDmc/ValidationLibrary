219041ffebe9d9de6428298c7ba611cbe2dbfc11
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-03-05 18:47:48 +0200

    all functions refactor refactor, range commented

--- validationLib.js
+++ validationLib.js
@@ -8,7 +8,7 @@ export function notEmptyValidation(value) {
     return true;
   }
 
-function isValidEmail (email) {
+export function isValidEmail (email) {
     let emailRegex = /^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
